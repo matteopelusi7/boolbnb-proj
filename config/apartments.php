f13a5413f59dd9c427c307a5e9c3898d3189d0b4
matteopelusi7 <pelusimatteo02@gmail.com>
2022-06-14 12:42:29 +0200

    add file database

---
+++ config/apartments.php
@@ -0,0 +1,224 @@
+<?php
+
+return [
+    [
+        "user_id" => "1",
+        "title" => "Appartamento sul mare",
+        "number_of_room" => "5",
+        "number_of_bedroom" => "2",
+        "number_of_bathroom" => "2",
+        "sqm" => "150mq",
+        "address" => "Via Roma, 40",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "1",
+        "title" => "Appartamento in montagna",
+        "number_of_room" => "2",
+        "number_of_bedroom" => "1",
+        "number_of_bathroom" => "1",
+        "sqm" => "80mq",
+        "address" => "Via Milano, 10",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "2",
+        "title" => "Casa vista mare",
+        "number_of_room" => "6",
+        "number_of_bedroom" => "2",
+        "number_of_bathroom" => "2",
+        "sqm" => "100mq",
+        "address" => "Via Ossi, 78",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "2",
+        "title" => "Villa Ursini",
+        "number_of_room" => "3",
+        "number_of_bedroom" => "1",
+        "number_of_bathroom" => "1",
+        "sqm" => "70mq",
+        "address" => "Via Albo, 15",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "3",
+        "title" => "Casa sul lago",
+        "number_of_room" => "10",
+        "number_of_bedroom" => "3",
+        "number_of_bathroom" => "3",
+        "sqm" => "170mq",
+        "address" => "Via Roma, 2",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "3",
+        "title" => "Appartamento in collina",
+        "number_of_room" => "5",
+        "number_of_bedroom" => "1",
+        "number_of_bathroom" => "1",
+        "sqm" => "80mq",
+        "address" => "Via Sante, 50",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "4",
+        "title" => "Appartamento in montagna con vista mare",
+        "number_of_room" => "6",
+        "number_of_bedroom" => "2",
+        "number_of_bathroom" => "2",
+        "sqm" => "100mq",
+        "address" => "Via Livorno, 85",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "4",
+        "title" => "Appartamento sul mare",
+        "number_of_room" => "10",
+        "number_of_bedroom" => "3",
+        "number_of_bathroom" => "2",
+        "sqm" => "200mq",
+        "address" => "Via Pisa, 25",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "5",
+        "title" => "Casa sul lago",
+        "number_of_room" => "5",
+        "number_of_bedroom" => "2",
+        "number_of_bathroom" => "2",
+        "sqm" => "70mq",
+        "address" => "Via Roma, 80",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "5",
+        "title" => "Casa vista mare",
+        "number_of_room" => "8",
+        "number_of_bedroom" => "3",
+        "number_of_bathroom" => "2",
+        "sqm" => "150mq",
+        "address" => "Via Milano, 70",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "6",
+        "title" => "Appartamento in montagna",
+        "number_of_room" => "7",
+        "number_of_bedroom" => "3",
+        "number_of_bathroom" => "3",
+        "sqm" => "125mq",
+        "address" => "Via Roma, 5",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "6",
+        "title" => "Appartamento sul mare",
+        "number_of_room" => "5",
+        "number_of_bedroom" => "2",
+        "number_of_bathroom" => "2",
+        "sqm" => "115mq",
+        "address" => "Via Como, 88",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "7",
+        "title" => "Villa vista mare",
+        "number_of_room" => "15",
+        "number_of_bedroom" => "10",
+        "number_of_bathroom" => "4",
+        "sqm" => "270mq",
+        "address" => "Via Garibaldi, 40",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "7",
+        "title" => "Villa Albertini",
+        "number_of_room" => "8",
+        "number_of_bedroom" => "4",
+        "number_of_bathroom" => "2",
+        "sqm" => "180mq",
+        "address" => "Via Da Vinci, 30",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "8",
+        "title" => "Casa in centro città",
+        "number_of_room" => "5",
+        "number_of_bedroom" => "2",
+        "number_of_bathroom" => "2",
+        "sqm" => "90mq",
+        "address" => "Via Salite, 92",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "8",
+        "title" => "Casa in periferia",
+        "number_of_room" => "4",
+        "number_of_bedroom" => "1",
+        "number_of_bathroom" => "1",
+        "sqm" => "70mq",
+        "address" => "Via Palermo, 15",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "9",
+        "title" => "Appartamento sul mare",
+        "number_of_room" => "8",
+        "number_of_bedroom" => "3",
+        "number_of_bathroom" => "2",
+        "sqm" => "190mq",
+        "address" => "Via Milano, 22",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "9",
+        "title" => "Casa in città",
+        "number_of_room" => "5",
+        "number_of_bedroom" => "2",
+        "number_of_bathroom" => "2",
+        "sqm" => "90mq",
+        "address" => "Via Catania, 30",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "10",
+        "title" => "Casa in campagna",
+        "number_of_room" => "5",
+        "number_of_bedroom" => "2",
+        "number_of_bathroom" => "2",
+        "sqm" => "90mq",
+        "address" => "Via Pini, 77",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+    [
+        "user_id" => "10",
+        "title" => "Villa in centro città",
+        "number_of_room" => "12",
+        "number_of_bedroom" => "6",
+        "number_of_bathroom" => "3",
+        "sqm" => "175mq",
+        "address" => "Via Doria, 6",
+        "cover" => "https://picsum.photos/200/300",
+        "visible" => true
+    ],
+];
---
+++ config/user.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+    [
+        "email" => "paxine5145@doerma.com",
+        "password" => "1234",
+        "name" => "Marco",
+        "surname" => "Rossi",
+        "date_of_birth" => "15/07/1999",
+    ],
+    [
+        "email" => "wanon70822@cupbest.com",
+        "password" => "1234",
+        "name" => "Andrea",
+        "surname" => "Brighenti",
+        "date_of_birth" => "4/05/2000",
+    ],
+    [
+        "email" => "wigel64467@doerma.com",
+        "password" => "1234",
+        "name" => "Matteo",
+        "surname" => "Magnanelli",
+        "date_of_birth" => "15/04/1982",
+    ],
+    [
+        "email" => "dobeg66940@cupbest.com",
+        "password" => "1234",
+        "name" => "Giovanna",
+        "surname" => "Arese",
+        "date_of_birth" => "25/05/1985",
+    ],
+    [
+        "email" => "paxina5178@doerma.com",
+        "password" => "1234",
+        "name" => "Costantino",
+        "surname" => "D'Alessandro",
+        "date_of_birth" => "30/04/1998",
+    ],
+    [
+        "email" => "fixine55@doerma.com",
+        "password" => "1234",
+        "name" => "Marco",
+        "surname" => "Bielli",
+        "date_of_birth" => "1/10/1980",
+    ],
+    [
+        "email" => "mojeno2228@doerma.com",
+        "password" => "1234",
+        "name" => "Andrea",
+        "surname" => "Verdi",
+        "date_of_birth" => "30/05/1995",
+    ],
+    [
+        "email" => "nameyeh418@doerma.com",
+        "password" => "1234",
+        "name" => "Giuseppe",
+        "surname" => "D'Angelo",
+        "date_of_birth" => "5/04/2002",
+    ],
+    [
+        "email" => "pnameuse77@doerma.com",
+        "password" => "1234",
+        "name" => "Alberto",
+        "surname" => "Mastro",
+        "date_of_birth" => "20/12/1963",
+    ],
+    [
+        "email" => "demew11458@doerma.com",
+        "password" => "1234",
+        "name" => "Antonio",
+        "surname" => "Russo",
+        "date_of_birth" => "28/11/1958",
+    ],
+];
